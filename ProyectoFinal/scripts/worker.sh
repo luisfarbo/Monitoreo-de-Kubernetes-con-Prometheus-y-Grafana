d3a85890f0328020e82ac52bca11b30b2a9e3517
luisfarbo <luis_f.arboleda@uao.edu.co>
2025-11-12 17:48:40 -0500

    Add files via upload

---
+++ ProyectoFinal/scripts/worker.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#
+# Setup de Worker Nodes
+#
+
+set -euxo pipefail
+
+echo "Esperando a que el master genere join command..."
+
+# Esperar hasta que el archivo exista
+while [ ! -f /vagrant/join-command.sh ]; do
+  sleep 5
+done
+
+echo "Join command encontrado, uniéndose al cluster..."
+
+# Unirse al cluster
+sudo bash /vagrant/join-command.sh
+
+echo "Worker node unido al cluster exitosamente"
